595e14c80d10bd032c148f9033030144fb4d89ce
Sebastian Hauer <shauer@steelperlot.com>
2024-03-21 21:52:39 -0400

    [mem] Added fixed storage unit tests.

--- include/sh/mem/storage.hpp
+++ include/sh/mem/storage.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstddef>
+#include <memory>
 #include <new>
 
 namespace sh::mem {
@@ -10,23 +11,31 @@ constexpr std::size_t cache_line_alignment = 64;
 
 template <std::size_t N, std::size_t Alignment = cache_line_alignment>
 struct fixed_storage {
+    fixed_storage() noexcept : data_ptr_{std::data(data_)} {}
 
-    std::size_t remaining() const noexcept { return N - pos_; }
-
-    void* allocate(std::size_t n, std::size_t align) {
-        if (n > remaining()) {
-            throw std::bad_array_new_length{};
-        }
-        if (align > Alignment) {
-            return std::bad_alloc{};
-        }
-        auto ptr = &data_[pos_];
-        pos_ += size;
-        return ptr;
+    constexpr const std::byte* data() const noexcept {
+        return std::data(data_);
     }
 
+    constexpr std::size_t max_size() const noexcept { return N; }
+    std::size_t remaining() const noexcept { return rem_size_; }
+    std::size_t used() const noexcept { return N - rem_size_; }
+
+    void* allocate(const std::size_t n, const std::size_t align) {
+        if (void* ptr = data_ptr_; std::align(align, n, ptr, rem_size_)) {
+            data_ptr_ = static_cast<std::byte*>(ptr) + n;
+            rem_size_ -= n;
+            return ptr;
+        }
+        throw std::bad_alloc{};
+    }
+
+    void deallocate(void* const ptr, const std::size_t n,
+                    const std::size_t align) {}
+
   private:
-    std::size_t pos_{};
+    std::size_t rem_size_{N};
+    std::byte* data_ptr_{};
     alignas(Alignment) std::array<std::byte, N> data_{};
 };
 
